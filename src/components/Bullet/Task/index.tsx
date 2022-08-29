83075308ed2bf4253748316805ef9254fb0b855d
Nikoly Cover Pereira <nikoly.cp@pti.org.br>
2022-08-29 09:33:45 -0300

    creating popover for tasks

--- src/components/Bullet/Task/index.tsx
+++ src/components/Bullet/Task/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Task.module.scss'
 import { MoreHorizontal } from 'react-feather'
 
 const Task: React.FC = () => {
+    const [optionsOpen, setOptionsOpen] = useState(false) 
+
     return (
         <div className={styles.task}>
-            <MoreHorizontal className={styles.moreIcon} size={16} />
+            <button className={styles.moreIcon} onClick={() => setOptionsOpen(!optionsOpen)} onBlur={() => setOptionsOpen(false)} >
+                <MoreHorizontal size={16}/>
+            </button>
+            {
+                optionsOpen && (
+                    <div className={styles.popover}>
+                        <button>Editar</button>
+                        <button>Apagar</button>
+                    </div>
+                )
+            }
             <h1>Lorem Ipsum</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
